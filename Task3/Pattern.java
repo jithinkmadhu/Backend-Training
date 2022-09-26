b0ae71b185d3f0d3405b7cea732c68538d6f68c4
jithinkmadhu <jithinmadhu00@gmail.com>
2022-09-26 16:10:33 +0530

    Update Pattern.java

--- Task3/Pattern.java
+++ Task3/Pattern.java
@@ -2,7 +2,7 @@ package com.orthofx;
 
 import java.util.*;
 
-public class PatternThree {
+public class Pattern {
 
 	static int findIndexOfOSB(char arr[]) {
 		for(int i=arr.length-1;i>-1;i--){    
@@ -39,41 +39,49 @@ public class PatternThree {
 		String pattern = "";
 		String fullPattern = "";
 		String nextArray = "";
+		String howManyString = "";
+		String rev="";
 		char arr[] = new char[100];
 		
 		indexOfFirstOSB = findIndexOfOSB(array);
 		indexOfCSB = findIndexOfCSB(array, indexOfFirstOSB);
 
-		howMany = Character.getNumericValue(array[indexOfFirstOSB-1]);
+		for(int k = 1;k >= 0;k++) {
+			if(indexOfFirstOSB-k < 0 ) {
+				break;
+			}
+			if(array[indexOfFirstOSB-k] >= '0' && array[indexOfFirstOSB-k] <= '9') {
+				howManyString = howManyString + array[indexOfFirstOSB-k];
+			} else {
+				break;
+			}
+		}
+		
+		for(int i = howManyString.length()-1;i>=0;i--) {
+			rev += howManyString.charAt(i);
+		}
+		
+		howMany = Integer.parseInt(rev);
 		
 		for(int i = indexOfFirstOSB + 1; i < indexOfCSB; i++) {
 			pattern += array[i];
 		}
 		
-//		System.out.println("howMany = " + howMany);
-//		System.out.println("pattern = " + pattern);
-//		System.out.println(array);
 		for(int i = 0; i < howMany; i++) {
 			fullPattern = fullPattern + pattern;
 		}
-//		System.out.println("fullPattern = " + fullPattern);
 		
-		for(int i = 0; i < indexOfFirstOSB-1;i++) {
+		for(int i = 0; i < indexOfFirstOSB-(howManyString.length());i++) {
 			nextArray = nextArray + array[i];
 		}
-//		System.out.println("nextArray = " + nextArray);
+
 		nextArray = nextArray + fullPattern;
 		
-//		System.out.println("nextArray = " + nextArray);
 		for(int i = indexOfCSB+1; i < array.length;i++) {
 			nextArray = nextArray + array[i];
 		}
 		
 		arr = nextArray.toCharArray();
-		
-//		System.out.println("nextArray = " + nextArray);
-		
-//		System.out.println(arr);
 		
 		if(findIndexOfOSB(arr) != -1)
 			makePattern(arr);
