123fcaefdafe1edbed2aae0dabbeb600bd92fea9
jithinkmadhu <jithinmadhu00@gmail.com>
2022-09-26 11:58:52 +0530

    Update Pattern.java

--- Task3/Pattern.java
+++ Task3/Pattern.java
@@ -2,46 +2,106 @@ package com.orthofx;
 
 import java.util.*;
 
-public class Pattern {
-	
-	public static int findIndex(char arr[], char t)
-    {
-        int index = Arrays.binarySearch(arr, t);
-        return (index < 0) ? -1 : index;
-    }
+public class PatternThree {
+
+	static int findIndexOfOSB(char arr[]) {
+		for(int i=arr.length-1;i>-1;i--){    
+            if(arr[i] == '['){    
+                return i;    
+            }    
+        }    
+        return -1; 
+	}
+
+	static int findIndexOfCSB(char arr[], int indexOfOSB) {
+		int i;
+
+		Stack<Integer> st = new Stack<>();
+
+		for (i = indexOfOSB; i < arr.length; i++) {
+
+			if (arr[i] == '[') {
+				st.push((int) arr[i]);
+			} else if (arr[i] == ']') {
+				st.pop();
+				if (st.empty()) {
+					return i;
+				}
+			}
+		}
+
+		return -1;
+
+	}
+
+	static int makePattern(char array[]) {
+		int indexOfFirstOSB, indexOfCSB, howMany;
+		String pattern = "";
+		String fullPattern = "";
+		String nextArray = "";
+		char arr[] = new char[100];
+		
+		indexOfFirstOSB = findIndexOfOSB(array);
+		indexOfCSB = findIndexOfCSB(array, indexOfFirstOSB);
+
+		howMany = Character.getNumericValue(array[indexOfFirstOSB-1]);
+		
+		for(int i = indexOfFirstOSB + 1; i < indexOfCSB; i++) {
+			pattern += array[i];
+		}
+		
+//		System.out.println("howMany = " + howMany);
+//		System.out.println("pattern = " + pattern);
+//		System.out.println(array);
+		for(int i = 0; i < howMany; i++) {
+			fullPattern = fullPattern + pattern;
+		}
+//		System.out.println("fullPattern = " + fullPattern);
+		
+		for(int i = 0; i < indexOfFirstOSB-1;i++) {
+			nextArray = nextArray + array[i];
+		}
+//		System.out.println("nextArray = " + nextArray);
+		nextArray = nextArray + fullPattern;
+		
+//		System.out.println("nextArray = " + nextArray);
+		for(int i = indexOfCSB+1; i < array.length;i++) {
+			nextArray = nextArray + array[i];
+		}
+		
+		arr = nextArray.toCharArray();
+		
+//		System.out.println("nextArray = " + nextArray);
+		
+//		System.out.println(arr);
+		
+		if(findIndexOfOSB(arr) != -1)
+			makePattern(arr);
+		else {
+			System.out.print("Pattern : ");
+			System.out.println(arr);
+		}
+			
+		return 0;
+
+	}
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-		int indexOfOpeningSquareBracket,multiplier;
-		char array[] = new char[1];
-		String inputRegEx, multi = "";
-		String pattern = "";
-		
-		System.out.print("Enter the pattern : ");
-		inputRegEx = sc.next();
-		System.out.println(array.length);
-		array = inputRegEx.toCharArray();
-		System.out.println(array.length);
-		indexOfOpeningSquareBracket = findIndex(array,'[');
-		
-		for(int i = 0; i < indexOfOpeningSquareBracket; i++) {
-			multi += array[i];
-		}
-		
-		multiplier = Integer.parseInt(multi);
-		
-		for(int i=indexOfOpeningSquareBracket+1;i<array.length-1;i++) {
-			pattern += array[i];
-		}                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-		
-		for(int i=0;i<multiplier;i++) {
-			System.out.print(pattern);
-		}  
 
-        sc.close();
-		
+		String inputRegEx;
+		char regExArray[] = new char[1];
+
+		System.out.print("Enter the Regular Expression : ");
+		inputRegEx = sc.next();
+
+		regExArray = inputRegEx.toCharArray();
+
+		makePattern(regExArray);
+
+		sc.close();
+
 	}
 
 }
