436ae34cf5b540245ba58c474755bc084f3915a1
jithinkmadhu <jithinmadhu00@gmail.com>
2022-10-12 15:45:18 +0530

    reduce the no.of for loops

--- Task5/Diamond.java
+++ Task5/Diamond.java
@@ -11,35 +11,32 @@ public class Diamond {
 		n = sc.nextInt();
 		sc.close();
 		
-		for ( int i = 0; i < n; i++) {
-			for ( int j = 0; j < n-i; j++) {
+		int space = n-1;
+		int star = 1;
+		for ( int i = 0; i < n; i++) { 
+			for ( int j = 0; j < space; j++) {
 				System.out.print(" ");
 			}
-			for ( int k = 0; k <= i; k++) {
+			space--;
+			for ( int k = 0; k < star; k++) {
 				System.out.print("*");
 			}
-			for ( int k = 0; k <= i-1; k++) {
-				System.out.print("*");
-			}
-				
+			star+=2;
 			System.out.println();
 		}
-		int space = 1;
+		space = 1;
+		star = star-4;
 		for ( int i = n-1; i > 0; i--) {
-			for ( int j = 0; j <= space; j++) {
+			for ( int j = 0; j < space; j++) {
 				System.out.print(" ");
 			}
 			space++;
-			for ( int k = i-1; k > 0; k--) {
+			for ( int k = 0; k < star; k++) {
 				System.out.print("*");
 			}
-			for ( int k = 0; k <= i-1; k++) {
-				System.out.print("*");
-			}
-				
+			star-=2;
 			System.out.println();
 		}
-
 	}
 
 }
